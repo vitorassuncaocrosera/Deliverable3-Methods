eb23647f42b387f3fa2e45f46041b0de4db7c51e
VITOR <VITOR@VITORC>
2020-02-04 17:35:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Deliverable3_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try catch to prevent coding erros
+            try
+            {
+                // Ask for user input
+                Console.WriteLine("Please enter your name:");
+                string userName = HelloName();
+
+                // Show message to user input
+                Console.WriteLine("Hello " + userName + "!");
+
+            }
+
+            catch
+            {   //If error in the code, the follow message will appear:
+                Console.WriteLine("An error was present in the code. Please try again. ");
+            }
+        }
+
+
+        private static string HelloName()
+        {
+            string UserName = Console.ReadLine();
+            return UserName;
+        }
+        }
+
+    } // End of Max_Heart_Rate
+    
