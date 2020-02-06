e98750008779fcfbc15e22ceb96b7e100fd946da
VITOR <VITOR@VITORC>
2020-02-06 18:05:21 -0500

    commit all

--- Program.cs
+++ Program.cs
@@ -5,43 +5,36 @@ namespace Deliverable3_Methods
     class Program
     {
         static void Main(string[] args)
-        {
-            
-          
-            
-                 // Ask for user to input his/her name
-                Console.WriteLine("Please enter your name:");
-                string Username;
-
-
+        {             
+            // Ask for user to input his/her name
+            Console.WriteLine("Please enter your name:");
+            string Username;
             Name();
-
-                void Name()            
-                {
-
-                   string Username = Console.ReadLine(); // collect user input
-                    if (System.Text.RegularExpressions.Regex.IsMatch(Username, "[a-zA-Z]" + " " + "[a-zA-Z]+$")) // test if user entered string or not
-                    {
-                        // display message to user
-                        Console.WriteLine("Hello" + " " + Username + "!");
-                        
-                    }
-                    else if (System.Text.RegularExpressions.Regex.IsMatch(Username, @"^[a-zA-Z]+$"))
-                    {
-                        // display message to user
-                        Console.WriteLine("Hello" + " " + Username + "!");
-                        
-                    }
-                    else
-                    {
-                        // display error message to user
-                        Console.WriteLine("Please only enter alphabetical letters.");
-                        Console.WriteLine("Please enter your name and try again.");
-                    }
-                }
-           
-
             
+        }
+
+        private static void Name()
+        {
+
+            string Username = Console.ReadLine(); // collect user input
+            if (System.Text.RegularExpressions.Regex.IsMatch(Username, "[a-zA-Z]" + " " + "[a-zA-Z]+$")) // test if user entered string or not
+            {
+                // display message to user
+                Console.WriteLine("Hello" + " " + Username + "!");
+
+            }
+            else if (System.Text.RegularExpressions.Regex.IsMatch(Username, @"^[a-zA-Z]+$"))
+            {
+                // display message to user
+                Console.WriteLine("Hello" + " " + Username + "!");
+
+            }
+            else
+            {
+                // display error message to user
+                Console.WriteLine("Please only enter alphabetical letters.");
+                Console.WriteLine("Please enter your name and try again.");
+            }
         }
 
     }
